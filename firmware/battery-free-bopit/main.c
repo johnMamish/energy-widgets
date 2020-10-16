7d82c3e199ef3a6ff08dfbe9a7270551ac30f9dc
John Mamish <john.mamish@gmail.com>
2020-10-15 21:42:25 -0500

    add random sample selection example to main.c

--- firmware/battery-free-bopit/main.c
+++ firmware/battery-free-bopit/main.c
@@ -10,26 +10,74 @@
 static void init_hardware();
 static void start_audio_dma(const uint8_t* src, int32_t len);
 
+const uint8_t* action_sounds[] =
+{
+    __assets_bop_1_wav, __assets_bop_2_wav, __assets_bop_3_wav, __assets_bop_4_wav,
+    __assets_shake_1_wav, __assets_shake_2_wav, __assets_shake_3_wav, __assets_shake_4_wav,
+    __assets_twist_1_wav, __assets_twist_2_wav, __assets_twist_3_wav, __assets_twist_4_wav
+};
+
+const uint8_t* it_sounds[] =
+{
+    __assets_it_1_wav, __assets_it_2_wav, __assets_it_3_wav, __assets_it_4_wav
+};
+
+static void advance_lfsr(uint32_t* num)
+{
+    int bit = *num & 1;
+    *num >>= 1;
+    if(bit) {
+        *num ^= ((1UL << 31) | (1UL << 29) | (1UL << 25) | (1UL << 24));
+    }
+}
+
 int main()
 {
     init_hardware();
 
+    uint32_t rando = 0xa5ce5b3a;
+
+    uint16_t eighth_note_delay = (1 << 15) - 100;
+    TA0CTL |= (1 << 2);
+
     while(1) {
-        start_audio_dma(__assets_Crash_Cymbal_1_wav, __assets_Crash_Cymbal_1_wav_size);
-        while(DMA1CTL & (1 << 4));
+        for (uint8_t i = 0; i < 16; i++, advance_lfsr(&rando));
 
-        // Delay for a little bit
-        //for (volatile uint32_t i = 0; i < 10000; i++);
+        // "bop it"
+        for (uint8_t i = 0; i < 4; i++, advance_lfsr(&rando));
+        int idx = (rando & 0x07);
+        for (uint8_t i = 0; i < 2; i++, advance_lfsr(&rando));
+        idx += (rando & 0x03);
+        TA0CTL |= (1 << 2);
+        start_audio_dma(action_sounds[idx], __assets_bop_1_wav_size);
+        //while(DMA1CTL & (1 << 4));
+        while(TA0R < (eighth_note_delay));
 
-        start_audio_dma(__assets_E_Mu_Proteus_FX_Wacky_Snare_wav, __assets_E_Mu_Proteus_FX_Wacky_Snare_wav_size/2);
-        while(DMA1CTL & (1 << 4));
+        for (uint8_t i = 0; i < 2; i++, advance_lfsr(&rando));
+        idx = rando & 0x03;
+        TA0CTL |= (1 << 2);
+        start_audio_dma(it_sounds[idx], __assets_it_3_wav_size);
+        while(TA0R < (eighth_note_delay));
 
+        // hi-hat
+        TA0CTL |= (1 << 2);
+        start_audio_dma(__assets_Closed_Hi_Hat_5_wav, __assets_Closed_Hi_Hat_5_wav_size);
+        while(TA0R < (eighth_note_delay * 2));
 
-        start_audio_dma(__assets_E_Mu_Proteus_FX_Wacky_Snare_wav, __assets_E_Mu_Proteus_FX_Wacky_Snare_wav_size/2);
-        while(DMA1CTL & (1 << 4));
+        // bass kicks
+        TA0CTL |= (1 << 2);
+        start_audio_dma(__assets_dry_kick_wav, __assets_dry_kick_wav_size);
+        while(TA0R < (eighth_note_delay));
+        TA0CTL |= (1 << 2);
+        start_audio_dma(__assets_dry_kick_wav, __assets_dry_kick_wav_size);
+        while(TA0R < (eighth_note_delay));
 
-        // Delay for a little bit
-        //for (volatile uint32_t i = 0; i < 10000; i++);
+        // hi-hat
+        TA0CTL |= (1 << 2);
+        start_audio_dma(__assets_Closed_Hi_Hat_5_wav, __assets_Closed_Hi_Hat_5_wav_size);
+        while(TA0R < (eighth_note_delay * 2));
+
+        eighth_note_delay -= 100;
     }
 
     return -1;
@@ -92,11 +140,17 @@ static void init_hardware()
     P3DIR  |=  (1 << 4);
 
     ////////////////////////////////////////////////////////////////
-    // TODO: Set up eUSCI
-
+    // Set up a timer
+    TA0CTL = ((0b10u    <<  8) |      // Use SMCLK for timer
+              (0b11u    <<  6) |      // Input divider is /8
+              (0b10u    <<  4) |      // Timer counts in continuous mode; rolls over from 0xffff to 0x0000.
+              (0b0u     <<  2) |      // Don't reset timer
+              (0b1u     <<  1) |      // interrupt enable
+              (0b0u     <<  0));      // interrupt pending flag
+    TA0EX0 = (0b111 << 0);            // Divide by 8 again for a frequency of 125kHz.
 
     ////////////////////////////////////////////////////////////////
-    // TODO: Set up DMA channel 1.
+    // Set up DMA channel 1 to copy from FRAM to timer
     // DMA channel 1 loads data from SPI RXBUF (UCxRXBUF) into TB0CCR3.
     // It should be triggered by TB0CCR0 CCIFG and have lower precedence than DMA channel 0.
     // Alternatively, it could be triggered by SPI TX complete.
